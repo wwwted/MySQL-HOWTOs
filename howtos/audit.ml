f0caa9cca5b7e62beb232e6325db322fa4a6101c
Ted Wennmark <ted.wennmark@gmail.com>
2018-02-06 09:01:15 +0100

    Create audit.ml

---
+++ howtos/audit.ml
@@ -0,0 +1,10 @@
+**[Back to Agenda](./../README.md)**
+
+# MySQL Enterprise Audit
+
+In this exercise we are going to create our environment, start mcm and configure our cluster.
+
+### Target environment for HOL
+```
+code
+```
